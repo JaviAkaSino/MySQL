2d377954476663e6499975da7411a5a4c3be0025
JaviAkaSino <javiakasino@hotmail.com>
2022-02-09 19:16:43 +0100

    Repaso

--- TEMA5/Simulacro 2.sql
+++ TEMA5/Simulacro 2.sql
@@ -1,9 +1,9 @@
 -- 1
 start transaction;
 insert into clientes
-	(codcli,nomcli,dnicli,tlf_contacto)
-value 
-	(899, "Juan del Campo Sánchez", "07000001W", "607000001");
+	(codcli, nomcli, ape1cli, ape2cli, dnicli, tlf_contacto)
+values 
+	(899, "Juan",  "del Campo", "Sánchez", "07000001W", "607000001");
 update reservas 
 	set codcliente = 899
     where codreserva = 4356;
@@ -14,10 +14,26 @@ delete from reservas
 	where codcliente = 456 and fecreserva = curdate();
     
 -- 3
-update clientes
+update propietarios
 	set tlf_contacto = '789000000',
 		correoelectronico = 'dfg@gmail.com'
 	where codcli = 789;
 
 -- 4
+start transaction;
+	delete from caracteristicasdecasas
+    where codcaracter = 230 or codcaracter = 245;
     
+    delete from caracteristicas
+    where numcaracter = 230 or codcaracter = 245;
+commit;
+   
+   /*Aquí podría hacerse directamente el segundo delete si se aplicara
+   on delete cascade. Al presumirse que está on delete no action por defecto,
+   esta sería la forma ya que, si no, se quedarían caracteristicasdecasas con 
+   campos nulos en su clave primaria*/
+   
+-- 5
+update casas
+set preciobase = preciobase * 1.1
+where numbanios = 3 and m2 = 200;

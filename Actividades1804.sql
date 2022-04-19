1ddda7051395b26c6ed10f8075d709e94c0fccb0
JaviAkaSino <javiakasino@hotmail.com>
2022-04-19 13:08:25 +0200

    Termina actividades 18

--- Actividades1804.sql
+++ Actividades1804.sql
@@ -1,91 +1,62 @@
+-- Javier Parodi
+
+use ventapromoscompleta;
+
 /*
 Prepara una función que, dado el código de una venta, devuelva el importe de dicha venta
 */
-use ventapromoscompleta;
-/*
-delimiter $$
-create procedure Proc_ejer_TAREA_18_1
-( in venta int, out importe decimal (8,2)
-)
-begin
-....
-end $$
-delimiter ;
 
-call Proc_ejer_TAREA_18_1 (1, @importe);
-select @importe;
-*/
-DROP FUNCTION IF EXISTS valorVenta;
-DELIMITER $$
-CREATE FUNCTION valorVenta
-    (venta int)
-  RETURNS DECIMAL(8,2)
-    LANGUAGE SQL
+drop function if exists importeVenta;
+delimiter $$
+create function importeVenta (venta int)
+
+RETURNS int
+	LANGUAGE SQL
 	DETERMINISTIC
-    reads sql data
-    
-    COMMENT 'MÓDULO: BASES DE DATOS - UNIDAD 6 - TAREA 18 DE ABRIL - EJERCICIO 1'
+    READS SQL DATA
+
 BEGIN
 
 	return (select sum(cant*precioventa)
-			from detalleventa
-			where codventa = venta
-            );
-    
+				from detalleventa
+                where codventa = venta);
 
-END $$
-DELIMITER ;
-/* 
-Obtén un listado de las ventas de este año en el que se muestre el código de la venta, la fecha de la venta, 
-el nombre y apellidos del cliente de la venta y el importe de la venta (utiliza la función del apartado anterior).
+END$$
 
-*/
-select codventa, date_format(fecventa, '%d/%m/%Y'), concat_ws(' ', nomcli, ape1cli, ape2cli), Fun_ejer_TAREA_18_1(codventa)
-from ventas join clientes on ventas.codcli = clientes.codcli
-where year(fecventa) = year(curdate());
-/*
-Prepara una función que devuelva la parte anterior a la arroba del email de un cliente dado
-*/
-/*
-delimiter $$
-create procedure Proc_ejer_TAREA_18_3
-( in cliente int, out nombre varchar(20)
-)
-begin
-....
-end $$
 delimiter ;
 
-call Proc_ejer_TAREA_18_3 (1, @nombre);
-select @nombre;
-*/
-DROP FUNCTION IF EXISTS Fun_ejer_TAREA_18_3;
-DELIMITER $$
-CREATE FUNCTION Fun_ejer_TAREA_18_3
-    (cliente int)
-  RETURNS varchar(20)
-    LANGUAGE SQL
+/*Obtén un listado de las ventas de este año en el que se muestre el código de la venta, la fecha de la venta,
+el nombre y apellidos del cliente de la venta y el importe de la venta (utiliza la función del apartado anterior).*/
+
+SELECT ventas.codventa as 'Venta', date_format(ventas.fecventa, '%d/%m/%Y') as 'Fecha', 
+	concat_ws(' ', clientes.nomcli, clientes.ape1cli, clientes.ape2cli) as 'Cliente', 
+    importeVenta(ventas.codventa) as 'Total'
+FROM ventas JOIN clientes ON ventas.codcli = clientes.codcli
+WHERE year(ventas.fecventa) = year(curdate());
+
+
+/*Prepara una función que devuelva la parte anterior a la arroba del email de un cliente dado*/
+
+drop function if exists textoEmail;
+delimiter $$
+create function textoEmail (cliente int)
+
+RETURNS varchar(100)
+	LANGUAGE SQL
 	DETERMINISTIC
-    reads sql data
-    
-    COMMENT 'MÓDULO: BASES DE DATOS - UNIDAD 6 - TAREA 18 DE ABRIL - EJERCICIO 3'
+    READS SQL DATA
+
 BEGIN
--- select Fun_ejer_TAREA_18_3(1);
-/*	return (select ifnull(substring_index(email,'@',1), ' no tiene correo')
-			from clientes
-			where codcli = cliente
-            );*/
-    return (select ifnull(substring(email,1,locate('@',email)-1), ' no tiene correo')
-			from clientes
-			where codcli = cliente
-            );
-    
 
-END $$
-DELIMITER ;
+	return (select substring(email, 1, (locate('@', email))-1)
+				from clientes
+                where codcli = cliente);
 
-/* PROPUESTO PARA 19 DE ABRIL:
-Obtener un listado de clientes  (código de cliente, nombre y apellidos) 
-y el importe de las ventas que han hecho dichos  clientes
-Si un cliente no tiene ventras debe mostrrase
-*/
+END$$
+
+delimiter ;
+
+-- Prueba 
+
+SELECT *, textoEmail(codcli)
+FROM clientes;

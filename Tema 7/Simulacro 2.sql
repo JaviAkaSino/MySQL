cfb99d05f71ca06715b417fef15256af0810f376
JaviAkaSino <javiakasino@hotmail.com>
2022-05-03 20:53:57 +0200

    Sigue intentando simulacro

--- Tema 7/Simulacro 2.sql
+++ Tema 7/Simulacro 2.sql
@@ -80,9 +80,9 @@ create procedure cursorVentasCliente(
 BEGIN
 	-- Orden de declaración: Variables, cursor y manejador
     -- VARIABLES
-    DECLARE nombre_cliente, nombre_articulo, cliente_auxiliar, mes_venta, mes_auxiliar varchar(100);
+    DECLARE nombre_cliente, nombre_articulo, cliente_auxiliar, mes_venta, mes_auxiliar varchar(100) DEFAULT '';
     DECLARE unidades_compradas int DEFAULT 0; 
-    DECLARE precio_compra decimal(7,5) DEFAULT 0;
+    DECLARE precio_compra decimal(7,5) DEFAULT 0.00;
  
     DECLARE mismo_cliente, primera_fila boolean DEFAULT 1;
     DECLARE fin_cursor boolean DEFAULT 0;
@@ -107,7 +107,10 @@ BEGIN
 			SET fin_cursor = 1;
         END;
 	
-    DROP TEMPORARY TABLE IF EXISTS listado;
+    
+    
+    DROP TABLE IF EXISTS listado;
+    
     CREATE TEMPORARY TABLE listado (filarecorrida varchar(500)); -- Crea la tabla temporal
     
     OPEN cursorClientes; -- Abre el cursor
@@ -118,9 +121,9 @@ BEGIN
 		VALUES(concat('COMPRAS DE CLIENTES ENTRE: ', 
         upper(monthname(convert(concat('2020/', mes1, '/1'),date))),
         ' Y ', upper(monthname(convert(concat('2020/', mes2, '/1'), date))))),
-        ('------------------------------------------------------------------');
+        ('-----------------------------------------------------------------');
     
-    WHILE fin_cursor = 0 DO -- Mientras no se acaben los registros
+    WHILE NOT fin_cursor DO -- Mientras no se acaben los registros
 		BEGIN
 
 			IF nombre_cliente <> cliente_auxiliar THEN -- Cuando el nombre cambie, cabecera nueva
@@ -128,7 +131,7 @@ BEGIN
                 
                 INSERT INTO listado -- Pone el nombre del cliente
 							VALUES 
-                            (concat('CLIENTE: ', concat_ws(' ', clientes.nomcli, clientes.ape1cli, clientes.ape2cli))),
+                            (concat('CLIENTE: ', nombre_cliente)),
                             ('------------------------------------------------------------------------------------'),
                             (upper(monthname(convert(concat('2020/', mes1, '/1'),date))));
 							
@@ -186,3 +189,198 @@ delimiter ;
 
 
 call cursorVentasCliente(1,3);
+
+
+
+
+
+use ventapromoscompleta;
+delimiter $$
+drop procedure if exists ExamU7G1 $$
+create procedure ExamU7G1 (
+	in mesini int,
+    in mesfin int)
+begin
+	-- call ExamU7G1(1,6);
+	declare nomCliente, nomClienteAux, nomClienteAnterior varchar(100) default '';
+    declare codigoCliente, codigoClienteAux int default 0;
+    declare mesVenta, mesVentaAux varchar(12) default '';
+    declare nombreArticulo varchar(100) default '';
+    declare numVentas, cantVenta, sumCantVentas int default 0;
+    declare precVenta, sumPrecVentas decimal(7,2) default 0;
+    declare mediaVentaEmpresa decimal(7,2) default 0;
+    declare puntosRegalados int default 0;
+    
+    declare primeraIteracion boolean default true;
+    declare finalcursor boolean default false;
+    
+    declare curClientes cursor for
+						select clientes.codcli, concat_ws(' ', clientes.nomcli, clientes.ape1cli, clientes.ape2cli),
+								monthname(ventas.fecventa), articulos.nomart, detalleventa.cant, detalleventa.precioventa
+						from ventas join clientes on ventas.codcli=clientes.codcli
+							join detalleventa on ventas.codventa = detalleventa.codventa
+								join articulos on detalleventa.refart = articulos.refart
+						where month(ventas.fecventa)  between 1 and 6 --  mesini and mesfin
+-- 							and year(ventas.fecventa) = year(curdate())
+						order by concat_ws(' ', clientes.nomcli, clientes.ape1cli, clientes.ape2cli), 
+							month(ventas.fecventa);
+	
+    declare continue handler FOR SQLSTATE '02000' 
+				SET finalcursor = true;
+/*	declare exit handler FOR sqlexception
+				begin
+					rollback;
+                    close curClientes;
+                end;
+*/ -- 	start transaction;
+    
+    drop table if exists listado;
+	create temporary table listado
+		(descripcion varchar(200));
+    
+	open curClientes;
+    fetch next from curClientes into codigoCliente, nomCliente, mesVenta, nombreArticulo, cantVenta, precVenta;
+    
+    INSERT INTO listado 
+            -- (descripcion) 
+        VALUES 
+            (CONCAT('COMPRAS DE CLIENTES DE: ',  
+					upper(monthname(convert(concat('2020/',mesini,'/1'),date))), ' A ', 
+                     upper(monthname(convert(concat('2020/',mesfin,'/1'),date)))
+					)
+			),
+            ('--------------------------------------------------------------------------------------------------------------');
+        
+    while not finalcursor do
+    begin
+		if nomCliente <> nomClienteAux then
+        begin
+			if primeraIteracion = false then
+			begin
+				if sumCantVentas/numVentas >= mediaVentaEmpresa then
+                begin
+					set puntosRegalados = 5;
+                    update clientes
+					set ptosacumulados = ptosacumulados + 5
+                    where codcli = codigoClienteAux;
+                    
+				end;
+                else
+					set puntosRegalados = 0;
+                end if;
+				INSERT INTO listado 
+					-- (descripcion) 
+				VALUES
+					('--------------------------------------------------------------------------------------------------------------'),
+					(concat_ws(' ', 'Media de ', nomClienteAux, ' en ', mesVentaAux, ':       ',	
+							sumCantVentas/numVentas),'------',   puntosRegalados),
+					('--------------------------------------------------------------------------------------------------------------');
+				set primeraIteracion = false;
+                set numVentas = 0;
+                set sumCantVentas = 0;
+                set SumPrecVentas = 0;
+                set codigoClienteAux = codigoCliente;
+			end;
+            else
+				set primeraIteracion = false;
+			end if;
+			INSERT INTO listado 
+            -- (descripcion) 
+			VALUES
+				(concat('CLIENTE: ', nomCliente)),
+				('--------------------------------------------------------------------------------------------------------------');
+			set nomClienteAux = nomCliente;
+            set mesVentaAux = ''; -- inicializo la variable
+        end;
+        end if;
+    
+		if mesVenta <> mesVentaAux then
+        
+        begin
+			if not primeraIteracion and mesVentaAux <> '' then
+			begin
+				if sumCantVentas/numVentas >= mediaVentaEmpresa then
+                begin
+					set puntosRegalados = 5;
+                    update clientes
+					set ptosacumulados = ptosacumulados + 5
+                    where codcli =  codigoClienteAux;
+                    
+				end;
+                else
+					set puntosRegalados = 0;
+                end if;
+				INSERT INTO listado 
+					-- (descripcion) 
+				VALUES
+					('--------------------------------------------------------------------------------------------------------------'),
+					(concat_ws(' ', 'Media de ', nomClienteAux, ' en ', mesVentaAux, ':       ',	
+							sumCantVentas/numVentas),'------', puntosRegalados),
+					('--------------------------------------------------------------------------------------------------------------');
+				set primeraIteracion = false;
+                set numVentas = 0;
+                set sumCantVentas = 0;
+                set SumPrecVentas = 0;
+                set codigoClienteAux = codigoCliente;
+			end;
+			end if;
+            
+			INSERT INTO listado 
+            -- (descripcion) 
+			VALUES
+				(upper(mesVenta)),
+                ('ArtÃƒÂ­culo               			-	            	Unidades compradas                     			Precio de compra');
+
+			set mesVentaAux = mesVenta;
+            -- calculo la media de unidades vendidas por venta en la empresa en ese mes:
+			set mediaVentaEmpresa = (select sum(cant)/count(*) 
+							from detalleventa join ventas on detalleventa.codventa = ventas.codventa
+							where month(ventas.fecventa) = mesVenta
+-- 								and year(ventas.fecventa) = year(curdate()
+							);
+		end;
+        end if;
+        
+        INSERT INTO listado 
+            -- (descripcion) 
+		VALUES
+				(concat(nombreArticulo, '               ',cantVenta,'                   ', precVenta));
+
+		set numVentas = numVentas+1;
+        set sumCantVentas = sumCantVentas + cantVenta;
+        
+		fetch next from curClientes into codigoCliente, nomCliente, mesVenta, nombreArticulo, cantVenta, precVenta;
+    end;
+    end while;
+    
+    close curClientes;
+    if (select count(*) from listado) > 0 then
+    begin
+		if sumCantVentas/numVentas >= mediaVentaEmpresa then
+		begin
+			set puntosRegalados = 5;
+			update clientes
+			set ptosacumulados = ptosacumulados + 5
+			where codcli = codigoClienteAux;
+		end;
+        end if;
+		INSERT INTO listado 
+		-- (descripcion) 
+		VALUES
+			('--------------------------------------------------------------------------------------------------------------'),
+			(concat_ws(' ', 'Media de ', nomClienteAux, ' en ', mesVentaAux, ':       ',	
+					sumCantVentas/numVentas), puntosRegalados),
+			('--------------------------------------------------------------------------------------------------------------');
+		select * from listado;
+	end;
+    else
+		select CONCAT('No existen ventas entre los meses ',
+					   monthname(convert(concat('2020/',mesini,'/1'),date)), ' y ', 
+					   monthname(convert(concat('2020/',mesfin,'/1'),date))
+					 );
+	end if;
+	drop table if exists listado;
+--     commit;
+    
+end $$
+delimiter ;
